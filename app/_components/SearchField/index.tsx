9e8ba8e4debdbe70f093f00daecf76d94f3801d3
kawamoto <kawamotomailbox8940@gmail.com>
2024-09-24 21:36:04 +0900

    検索機能の作成途中

---
+++ app/_components/SearchField/index.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import styles from "./index.module.css";
+
+export default function SearchField() {
+  const router = useRouter();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const q = e.currentTarget.elements.namedItem("q");
+
+    // qのHTML要素がinput要素だったら
+    if (q instanceof HTMLInputElement) {
+      const params = new URLSearchParams();
+      params.set("q", q.value.trim());
+      router.push(`/news/search?${params.toString()}`);
+    }
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label className={styles.search}>
+          <Image
+            src="/search.svg"
+            alt="検索"
+            width={16}
+            height={16}
+            loading="eager"
+          ></Image>
+          <input
+            type="text"
+            name="q"
+            placeholder="キーワードを入力"
+            className={styles.searchInput}
+          />
+        </label>
+      </form>
+    </>
+  );
+}
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -2,6 +2,7 @@ import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "@/app/_components/NewsList";
 import Pagenation from "@/app/_components/Pagenation";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import SearchField from "../_components/SearchField";
 
 export default async function Page() {
   // const { contents: news } = await getNewsList();
@@ -12,6 +13,7 @@ export default async function Page() {
 
   return (
     <>
+      <SearchField></SearchField>
       <NewsList news={news} />
       <Pagenation totalCount={totalCount}></Pagenation>
     </>
