6944002b27977ab50d71b26e7898afd1e67fef59
n_kawamoto@sky-inet.ne.jp <n_kawamoto@sky-inet.ne.jp>
2024-10-03 08:51:39 +0900

    BASIC認証設定

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextResponse) {
-  console.log("middleware:" + request.url);
+// export function middleware(request: NextResponse) {
+//   console.log("middleware:" + request.url);
 
-  return NextResponse.next();
+//   return NextResponse.next();
+// }
+
+// export const config = {
+//   matcher: ["/about/:path*", "/dashboard/:path*"],
+// };
+
+import { createNextAuthMiddleware } from "nextjs-basic-auth-middleware";
+
+export const middleware = createNextAuthMiddleware();
+
+export const condig = {
+  matcher: ["/(.*)"],
 }
-
-export const config = {
-  matcher: ["/about/:path*", "/dashboard/:path*"],
-};
